02cabffdd06ce454e185bd106b749ec0853e2725
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-08-29 14:12:06 -0400

    Added to JavaScript files for HTML5shiv. Added references for custom assets in header.php

--- header.php
+++ header.php
@@ -12,12 +12,30 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<!-- Bootstrap -->
+	<link href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/font-awesome.min.css">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Neuton:300,400,700|Open+Sans:400,700" rel="stylesheet">
+
+	<?php wp_head(); ?>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/html5shiv-3.7.2.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/respond-1.4.2.min.js"></script>
+	<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
