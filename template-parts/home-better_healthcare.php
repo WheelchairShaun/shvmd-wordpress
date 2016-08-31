ed38fbd042ce89ace647bd61689838fb1f7f1a2c
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-08-31 13:09:50 -0400

    Added static stub to home-better_healthcare template part

--- template-parts/home-better_healthcare.php
+++ template-parts/home-better_healthcare.php
@@ -11,4 +11,27 @@
 
 ?>
 
+<!-- Better Healthcare -->
+<section id="better-healthcare">
+	<div class="container">
 
+		<div class="section-header">
+			<h2>Why Use a Concierge Doctor?</h2>
+		</div><!--/.section-header-->
+
+		<p class="lead">In these days of fast food, cell phones and the information superhighway, there are some things that are done better the old fashion way.  This includes healthcare.</p>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-sm-offset-0 col-md-6 col-lg-6">
+				<h3>Reason One</h3>
+				<p>Lorem ipsum dolor sit amet, nonumy sensibus partiendo quo et, duo autem mundi ut. No aliquip copiosae nam. Te qui dicit tantas deseruisse. Saperet debitis intellegat et sit.</p>
+			</div><!--/.col-->
+
+			<div class="col-xs-12 col-sm-6 col-sm-offset-0 col-md-6 col-lg-6">
+				<h3>Reason Two</h3>
+				<p>Lorem ipsum dolor sit amet, ut vim malorum virtute efficiantur. Appareat insolens at quo. At est reque forensibus, ea amet partiendo mel, eos movet electram iudicabit te. Mea ne error quando, pro eu dicunt corrumpit appellantur. Per perpetua theophrastus ea, malorum albucius persequeris cu sit, ne.</p>
+			</div><!--/.col-->
+		</div><!--/.row-->
+		
+	</div><!--/.container-->
+</section><!--/#better-healthcare-->
