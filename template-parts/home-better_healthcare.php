8fab8b6779f8daa90fd9cbaaad7043fc3586e9b6
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-08-31 13:33:22 -0400

    Added dynamic template features using advanced custom fields tohome-better_healthcare template part

--- template-parts/home-better_healthcare.php
+++ template-parts/home-better_healthcare.php
@@ -8,6 +8,12 @@
  */
 
 // Advanced Custom Fields
+$bh_headline				= get_field( 'bh_headline' );
+$bh_introduction			= get_field( 'bh_introduction' );
+$bh_reason_1_headline		= get_field( 'bh_reason_1_headline' );
+$bh_reason_1_paragraph		= get_field( 'bh_reason_1_paragraph' );
+$bh_reason_2_headline		= get_field( 'bh_reason_2_headline' );
+$bh_reason_2_paragraph		= get_field( 'bh_reason_2_paragraph' );
 
 ?>
 
@@ -16,20 +22,20 @@
 	<div class="container">
 
 		<div class="section-header">
-			<h2>Why Use a Concierge Doctor?</h2>
+			<h2><?php echo $bh_headline; ?></h2>
 		</div><!--/.section-header-->
 
-		<p class="lead">In these days of fast food, cell phones and the information superhighway, there are some things that are done better the old fashion way.  This includes healthcare.</p>
+		<p class="lead"><?php echo $bh_introduction; ?></p>
 
 		<div class="row">
 			<div class="col-xs-12 col-sm-6 col-sm-offset-0 col-md-6 col-lg-6">
-				<h3>Reason One</h3>
-				<p>Lorem ipsum dolor sit amet, nonumy sensibus partiendo quo et, duo autem mundi ut. No aliquip copiosae nam. Te qui dicit tantas deseruisse. Saperet debitis intellegat et sit.</p>
+				<h3><?php echo $bh_reason_1_headline; ?></h3>
+				<p><?php echo $bh_reason_1_paragraph; ?></p>
 			</div><!--/.col-->
 
 			<div class="col-xs-12 col-sm-6 col-sm-offset-0 col-md-6 col-lg-6">
-				<h3>Reason Two</h3>
-				<p>Lorem ipsum dolor sit amet, ut vim malorum virtute efficiantur. Appareat insolens at quo. At est reque forensibus, ea amet partiendo mel, eos movet electram iudicabit te. Mea ne error quando, pro eu dicunt corrumpit appellantur. Per perpetua theophrastus ea, malorum albucius persequeris cu sit, ne.</p>
+				<h3><?php echo $bh_reason_2_headline; ?></h3>
+				<p><?php echo $bh_reason_2_paragraph; ?></p>
 			</div><!--/.col-->
 		</div><!--/.row-->
 		
