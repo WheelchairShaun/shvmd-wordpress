8e5ec01aa992601f6285a9eabd0564f7c0da4a20
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-08-31 13:57:46 -0400

    Added static stub to home-member_benefits template part

--- template-parts/home-member_benefits.php
+++ template-parts/home-member_benefits.php
@@ -10,3 +10,45 @@
 // Advanced Custom Fields
 
 ?>
+
+<!-- Membership Benefits -->
+<section id="member-benefits">
+	<div class="container">
+		
+		<div class="section-header">
+			<h2>Membership Benefits</h2>
+		</div><!--/.section-header -->
+
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-1 col-lg-2 col-lg-offset-1 benefit">
+				<i class="fa fa-calendar fa-2x"></i>
+				<h4>Same day or next day appointments</h4>
+			</div><!--/.col-->
+
+			<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 benefit">
+				<i class="fa fa-stethoscope fa-2x"></i>
+				<h4>On time, unhurried appointments</h4>
+			</div><!--/.col-->
+
+			<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 benefit">
+				<i class="fa fa-mobile fa-2x"></i>
+				<h4>Easy access during and after business hours</h4>
+			</div><!--/.col-->
+
+			<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 benefit">
+				<i class="fa fa-medkit fa-2x"></i>
+				<h4>House calls when medically necessary</h4>
+			</div><!--/.col-->
+
+			<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 benefit">
+				<i class="fa fa-hospital-o fa-2x"></i>
+				<h4>Hospital care</h4>
+			</div><!--/.col-->
+			
+		</div><!--/.row-->
+
+		<p><a class="btn btn-lg btn-info" href="services.html">Click here to learn more &raquo;</a></p>
+
+	</div><!--/.container -->
+</section><!--/#member-benefits-->
