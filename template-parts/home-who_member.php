dc3f3b3c145788e48afe0814b76753dc64a3451e
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-08-31 13:39:27 -0400

    Added static stub to home-who_member template part

--- template-parts/home-who_member.php
+++ template-parts/home-who_member.php
@@ -12,4 +12,38 @@
 
 ?>
 
+<!-- Who Member -->
+<section id="who-member">
+	<div class="container">
+		
+		<div class="section-header">
+			<h2>Who Should Be a Member?</h2>
+		</div><!--/.section-header-->
 
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+
+				<h3>First Type of Member</h3>
+				<p>Lorem ipsum dolor sit amet, putent regione an pri, ea iusto eirmod minimum qui, ex vis amet malis fabellas. Ut duis rebum similique vis, eu veritus pertinax vel. Eos ut oblique omittam accommodare. Vocent splendide ad sit, mel rebum tacimates ocurreret ea.</p>
+				<p>Eu munere quaestio petentium duo, ad vel nulla argumentum dissentias. Cu eos nullam meliore contentiones, fugit principes id cum. In sit ullum inani laboramus, pro an eruditi reformidans, eam augue salutandi necessitatibus cu. At aliquid intellegebat qui, aperiri maluisset vis et, usu ut rebum tamquam. Eos oblique <strong>impedit sapientem cu, in vix falli indoctum</strong>.</p>
+
+				<h3>Second Type of Member</h3>
+				<p>Lorem ipsum dolor sit amet, putent regione an pri, ea iusto eirmod minimum qui, ex vis amet malis fabellas. Ut duis rebum similique vis, eu veritus pertinax vel. Eos ut oblique omittam accommodare. Vocent splendide ad sit, mel rebum tacimates ocurreret ea.</p>
+				<p>In sit ullum inani laboramus, pro an eruditi reformidans, eam augue salutandi necessitatibus cu. At aliquid intellegebat qui, <strong>aperiri maluisset vis et, usu ut rebum tamquam.</strong></p>
+
+				<h3>Third Type of Member</h3>
+				<p>Lorem ipsum dolor sit amet, vis populo aperiam interpretaris id, ad sed sale summo. Ad option periculis eum. Et eum dicit graece fuisset. Vix cu quod civibus, te tempor graeco eirmod sed.</p>
+				<p>Et quod minimum pericula vel, sit soleat causae cu. Eu mel voluptua mandamus definitionem, dolorum atomorum in vis. Ut est error persequeris, omnis veniam decore ei ius. <strong>His virtute aliquam cu. Sanctus percipitur te vix.</strong></p>
+
+				<h3>Fourth Type of Member</h3>
+				<p>Lorem ipsum dolor sit amet, ex nec facilis accusamus quaerendum, ea nec utinam aeterno albucius, qui wisi appetere vituperatoribus ad. <strong>Ea referrentur interpretaris nam, vix.</strong></p>
+
+				<h3>Fifth Type of Member</h3>
+				<p><strong>Lorem ipsum dolor sit amet, soluta admodum iudicabit ei quo, ei ius saperet incorrupte, nam ad liberavisse deterruisset.</strong> Nam eu meliore fuisset periculis. Eu recusabo.</p>
+				<p>Eu nam primis graeci. Ius nonumy putant aperiam eu, pri reque velit quaestio ex, quodsi dolorum nostrum eum ad. Soluta atomorum ut his, id illud doming cum, eum at legere iisque periculis. Idque ornatus disputationi sed ex, eu ius illud maluisset.</p>
+				
+			</div><!--/.col-->
+		</div><!--/.row-->
+
+	</div><!--/.container-->
+</section><!--/#who-member-->
