4c9c3ce117cd15b353c3af4551bae26106760594
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-09-03 13:50:36 -0400

    Edited page.php and content-page template part for our custom theme

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,35 +9,49 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<!-- Feature image -->
+<section class="page-title">
+	<?php the_title( '<h1 class="entry-title"><span>', '</span></h1>' ); ?>
+</section>
 
-	<div class="entry-content">
-		<?php
-			the_content();
+<!-- Main content -->
+<main id="main" role="main">
+	<section id="content">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="container">
+				<div class="row">
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'shvmd' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'shvmd' ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-## -->
+					<?php
+						the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'shvmd' ),
+							'after'  => '</div>',
+						) );
+					?>
+
+					<?php if ( get_edit_post_link() ) : ?>
+						<div class="entry-footer">
+							<?php
+								edit_post_link(
+									sprintf(
+										/* translators: %s: Name of current post */
+										esc_html__( 'Edit %s', 'shvmd' ),
+										the_title( '<span class="screen-reader-text">"', '"</span>', false )
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+							?>
+						</div><!-- .entry-footer -->
+					<?php endif; ?>
+
+					</div><!--/.col-->
+
+				</div><!--/.row-->
+			</div><!--/.container-->
+		</article><!-- #post-## -->
+	</section><!--/#primary-->
+</main>
