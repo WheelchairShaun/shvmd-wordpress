c6f0dfa2793ea4a98eaef5d2dd6076b9848b3d2f
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-09-05 13:59:45 -0400

    Formatted the 404 template

--- 404.php
+++ 404.php
@@ -9,56 +9,35 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<!-- Feature image -->
+<section class="page-title">
+	<h1 class="entry-title"><span><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shvmd' ); ?></span></h1>
+</section>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shvmd' ); ?></h1>
-				</header><!-- .page-header -->
+<!-- Main content -->
+<main id="main" role="main">
+	<section id="content" class="error-404 not-found">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'shvmd' ); ?></p>
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below?', 'shvmd' ); ?></p>
 
-					<?php
-						get_search_form();
+						<nav>
+							<p><a href="<?php echo home_url( '/'); ?>">Visit our home page.</a></p>
+							<p><a href="<?php echo home_url( '/#doctor'); ?>">Learn about Dr. Mercer.</a></p>
+							<p><a href="<?php echo home_url( '/services'); ?>">View our benefits and services.</a></p>
+							<p><a href="<?php echo home_url( '/faq'); ?>">See our most often answered questions.</a></p>
+							<p><a href="<?php echo home_url( '/new-patient-forms'); ?>">Download forms for new patients.</a></p>
+						</nav>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( shvmd_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'shvmd' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'shvmd' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</div><!-- .page-content -->
+				</div><!-- .col -->
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</section><!-- .error-404 -->
+</main><!-- #main -->
 
 <?php
 get_footer();
